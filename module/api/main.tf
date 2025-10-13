2f9bcad0e4bed0c3d3dc830d75b358203f87ffef
devzeuz <soceeenin@yahoo.com>
2025-10-13 05:02:47 -0700

    changes

--- module/api/main.tf
+++ module/api/main.tf
@@ -11,12 +11,12 @@ resource "aws_api_gateway_resource" "test_resource" {
 resource "aws_api_gateway_method" "test_method" {
     rest_api_id = aws_api_gateway_rest_api.test_api.id
   resource_id = aws_api_gateway_resource.test_resource.id
-  http_method = var.http_method
+  http_method = "POST"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "test_int" {
-  http_method = var.http_method
+  http_method = aws_api_gateway_method.test_method.http_method
   resource_id = aws_api_gateway_resource.test_resource.id
   rest_api_id = aws_api_gateway_rest_api.test_api.id
   type = "AWS_PROXY"
