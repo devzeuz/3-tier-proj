7bd5fbe6a6a805f025f285b70515f0b06e9b943f
devzeuz <soceeenin@yahoo.com>
2025-10-13 10:29:54 -0700

    c

--- module/api/main.tf
+++ module/api/main.tf
@@ -1,6 +1,6 @@
-# resource "aws_api_gateway_rest_api" "test_api" {
-#   name = "test_api"
-# }
+resource "aws_api_gateway_rest_api" "test_api" {
+  name = "test_api"
+}
 
 # resource "aws_api_gateway_resource" "test_resource" {
 #   rest_api_id = aws_api_gateway_rest_api.test_api.id
