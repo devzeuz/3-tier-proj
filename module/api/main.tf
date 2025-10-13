f36b39d438bb9bf34260ca2c484074d746e45f65
devzeuz <soceeenin@yahoo.com>
2025-10-13 02:22:57 -0700

    fifth_commit

--- module/api/main.tf
+++ module/api/main.tf
@@ -1,33 +1,33 @@
-resource "aws_api_gateway_rest_api" "test-api" {
-  name = "test-api"
+resource "aws_api_gateway_rest_api" "test_api" {
+  name = "test_api"
 }
 
-resource "aws_api_gateway_resource" "test-resource" {
-  rest_api_id = aws_api_gateway_rest_api.test-api.id
-  parent_id   = aws_api_gateway_rest_api.test-api.root_resource_id
+resource "aws_api_gateway_resource" "test_resource" {
+  rest_api_id = aws_api_gateway_rest_api.test_api.id
+  parent_id   = aws_api_gateway_rest_api.test_api.root_resource_id
   path_part   = "test"
 }
 
-resource "aws_api_gateway_method" "test-method" {
-    rest_api_id = aws_api_gateway_rest_api.test-api.id
-  resource_id = aws_api_gateway_resource.test-resource.id
+resource "aws_api_gateway_method" "test_method" {
+    rest_api_id = aws_api_gateway_rest_api.test_api.id
+  resource_id = aws_api_gateway_resource.test_resource.id
   http_method = "POST"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "test-int" {
-  http_method = aws_api_gateway_method.test-method.http_method
-  resource_id = aws_api_gateway_resource.test-resource.id
-  rest_api_id = aws_api_gateway_rest_api.test-api.id
+resource "aws_api_gateway_integration" "test_int" {
+  http_method = aws_api_gateway_method.test_method.http_method
+  resource_id = aws_api_gateway_resource.test_resource.id
+  rest_api_id = aws_api_gateway_rest_api.test_api.id
   type = "AWS_PROXY"
   uri = var.lambda_function_arn
 }
 
-resource "aws_api_gateway_deployment" "test-deploy" {
-    rest_api_id = aws_api_gateway_rest_api.test-api.id
+resource "aws_api_gateway_deployment" "test_deploy" {
+    rest_api_id = aws_api_gateway_rest_api.test_api.id
 
     triggers = {
-      redeployment = sha1(jsonencode(aws_api_gateway_integration.test-int))
+      redeployment = sha1(jsonencode(aws_api_gateway_integration.test_int))
     }
 
     lifecycle {
@@ -35,8 +35,8 @@ resource "aws_api_gateway_deployment" "test-deploy" {
     }
 }
 
-resource "aws_api_gateway_stage" "test-stage" {
-    deployment_id = aws_api_gateway_deployment.test-deploy.id
-    rest_api_id = aws_api_gateway_rest_api.test-api.id
+resource "aws_api_gateway_stage" "test_stage" {
+    deployment_id = aws_api_gateway_deployment.test_deploy.id
+    rest_api_id = aws_api_gateway_rest_api.test_api.id
     stage_name = "test"
 }
