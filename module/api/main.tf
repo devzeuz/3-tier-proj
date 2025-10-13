7a459811bc187fed6f684c43371a3172347de8e7
devzeuz <soceeenin@yahoo.com>
2025-10-13 05:15:39 -0700

    changes

--- module/api/main.tf
+++ module/api/main.tf
@@ -5,7 +5,7 @@ resource "aws_api_gateway_rest_api" "test_api" {
 resource "aws_api_gateway_resource" "test_resource" {
   rest_api_id = aws_api_gateway_rest_api.test_api.id
   parent_id   = aws_api_gateway_rest_api.test_api.root_resource_id
-  path_part   = "test"
+  path_part   = "resource"
 }
 
 resource "aws_api_gateway_method" "test_method" {
@@ -16,9 +16,9 @@ resource "aws_api_gateway_method" "test_method" {
 }
 
 resource "aws_api_gateway_integration" "test_int" {
+  rest_api_id = aws_api_gateway_rest_api.test_api.id
   http_method = aws_api_gateway_method.test_method.http_method
   resource_id = aws_api_gateway_resource.test_resource.id
-  rest_api_id = aws_api_gateway_rest_api.test_api.id
   type = "AWS_PROXY"
   uri = var.lambda_function_arn 
 }
