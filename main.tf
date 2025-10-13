0d93e2611af803a00c2b6d3c45582fa82d3e4541
devzeuz <soceeenin@yahoo.com>
2025-10-13 10:21:58 -0700

    changes

--- main.tf
+++ main.tf
@@ -12,9 +12,9 @@ module "lambda" {
   lambda_module_table_name = module.dynamodb.dynamodb_table_name
 }
 
-module "api" {
-  source              = "./module/api"
-  lambda_function_arn = module.lambda.lambda_invocation_arn
-  aws_region = var.aws_region
-}
+# module "api" {
+#   source              = "./module/api"
+#   lambda_function_arn = module.lambda.lambda_invocation_arn
+#   aws_region = var.aws_region
+# }
 
