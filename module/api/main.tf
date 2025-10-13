b135c78d1878a9894d14c57aa7f658fe8766b5bb
devzeuz <soceeenin@yahoo.com>
2025-10-13 10:39:46 -0700

    c

--- module/api/main.tf
+++ module/api/main.tf
@@ -15,13 +15,13 @@ resource "aws_api_gateway_method" "test_method" {
   authorization = "NONE"
 }
 
-# resource "aws_api_gateway_integration" "test_int" {
-#   rest_api_id = aws_api_gateway_rest_api.test_api.id
-#   http_method = aws_api_gateway_method.test_method.http_method
-#   resource_id = aws_api_gateway_resource.test_resource.id
-#   type = "AWS_PROXY"
-#   uri = local.function_uri
-# }
+resource "aws_api_gateway_integration" "test_int" {
+  rest_api_id = aws_api_gateway_rest_api.test_api.id
+  http_method = aws_api_gateway_method.test_method.http_method
+  resource_id = aws_api_gateway_resource.test_resource.id
+  type = "AWS_PROXY"
+  uri = local.function_uri
+}
 
 # resource "aws_api_gateway_deployment" "test_deploy" {
 #     rest_api_id = aws_api_gateway_rest_api.test_api.id
