059638d189a9fd0a40bfce1c5f49737965f66614
devzeuz <soceeenin@yahoo.com>
2025-10-13 04:58:21 -0700

    changes

--- module/api/main.tf
+++ module/api/main.tf
@@ -20,8 +20,7 @@ resource "aws_api_gateway_integration" "test_int" {
   resource_id = aws_api_gateway_resource.test_resource.id
   rest_api_id = aws_api_gateway_rest_api.test_api.id
   type = "AWS_PROXY"
-  uri = var.lambda_function_arn
-  
+  uri = var.lambda_function_arn 
 }
 
 resource "aws_api_gateway_deployment" "test_deploy" {
