946ca08ee988cd55f0b05efa62c9ed1f78c2e79c
devzeuz <soceeenin@yahoo.com>
2025-10-13 11:00:31 -0700

    api resource

--- module/api/main.tf
+++ module/api/main.tf
@@ -2,11 +2,11 @@ resource "aws_api_gateway_rest_api" "test_api" {
   name = "test_api"
 }
 
-# resource "aws_api_gateway_resource" "test_resource" {
-#   rest_api_id = aws_api_gateway_rest_api.test_api.id
-#   parent_id   = aws_api_gateway_rest_api.test_api.root_resource_id
-#   path_part   = "resource"
-# }
+resource "aws_api_gateway_resource" "test_resource" {
+  rest_api_id = aws_api_gateway_rest_api.test_api.id
+  parent_id   = aws_api_gateway_rest_api.test_api.root_resource_id
+  path_part   = "resource"
+}
 
 # resource "aws_api_gateway_method" "test_method" {
 #     rest_api_id = aws_api_gateway_rest_api.test_api.id
