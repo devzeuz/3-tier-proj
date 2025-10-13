2e5591f2cb80ad6041d49ab74fd6fa3ca109ec70
devzeuz <soceeenin@yahoo.com>
2025-10-13 06:00:34 -0700

    changes

--- module/api/main.tf
+++ module/api/main.tf
@@ -11,7 +11,7 @@ resource "aws_api_gateway_resource" "test_resource" {
 resource "aws_api_gateway_method" "test_method" {
     rest_api_id = aws_api_gateway_rest_api.test_api.id
   resource_id = aws_api_gateway_resource.test_resource.id
-  http_method = "POST"
+  http_method = var.http_method
   authorization = "NONE"
 }
 
