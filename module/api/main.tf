ed17ff605c0e3f29c9380d7adca5fef6ecf671b2
devzeuz <soceeenin@yahoo.com>
2025-11-11 05:12:52 -0800

    6

--- module/api/main.tf
+++ module/api/main.tf
@@ -19,58 +19,6 @@ resource "aws_api_gateway_method" "test_method" {
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_method" "options_method" {
-  rest_api_id   = aws_api_gateway_rest_api.test_api.id
-  resource_id   = aws_api_gateway_resource.test_resource.id
-  http_method   = "OPTIONS"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "options_integration" {
-  rest_api_id = aws_api_gateway_rest_api.test_api.id
-  resource_id = aws_api_gateway_resource.test_resource.id
-  http_method = aws_api_gateway_method.options_method.http_method
-  type        = "MOCK"
-  request_templates = {
-    "application/json" = "{\"statusCode\": 200}"
-  }
-}
-
-resource "aws_api_gateway_method_response" "options_response" {
-  rest_api_id = aws_api_gateway_rest_api.test_api.id
-  resource_id = aws_api_gateway_resource.test_resource.id
-  http_method = aws_api_gateway_method.options_method.http_method
-  status_code = "200"
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = true
-    "method.response.header.Access-Control-Allow-Methods" = true
-    "method.response.header.Access-Control-Allow-Origin"  = true
-  }
-}
-
-resource "aws_api_gateway_integration_response" "options_integration_response" {
-  rest_api_id = aws_api_gateway_rest_api.test_api.id
-  resource_id = aws_api_gateway_resource.test_resource.id
-  http_method = aws_api_gateway_method.options_method.http_method
-  status_code = aws_api_gateway_method_response.options_response.status_code
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
-    "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,PUT,DELETE,OPTIONS'"
-    "method.response.header.Access-Control-Allow-Origin"  = "'https://d148sam9dykngp.cloudfront.net'"
-  }
-  depends_on = [aws_api_gateway_integration.options_integration]
-}
-
-resource "aws_api_gateway_method_response" "test_method_response" {
-  rest_api_id = aws_api_gateway_rest_api.test_api.id
-  resource_id = aws_api_gateway_resource.test_resource.id
-  http_method = aws_api_gateway_method.test_method.http_method
-  status_code = "200"
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = true
-  }
-}
-
 resource "aws_api_gateway_integration" "test_int" {
   depends_on = [ aws_api_gateway_method.test_method ]
   rest_api_id = aws_api_gateway_rest_api.test_api.id
@@ -89,8 +37,6 @@ resource "aws_api_gateway_deployment" "test_deploy" {
         aws_api_gateway_integration.test_int.id,
         aws_api_gateway_method.test_method.id,
         aws_api_gateway_resource.test_resource.id,
-        aws_api_gateway_integration.options_integration.id,
-        aws_api_gateway_method.options_method.id,
       ]))
     }
 
