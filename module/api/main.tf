5664195a9edffcd01056806c26dcd9933bcd1f89
devzeuz <soceeenin@yahoo.com>
2025-11-11 10:52:32 -0800

    6

--- module/api/main.tf
+++ module/api/main.tf
@@ -29,6 +29,56 @@ resource "aws_api_gateway_integration" "test_int" {
   uri = var.lambda_function_arn
 }
 
+
+#CORS Preflight (OPTIONS Method)
+resource "aws_api_gateway_method" "options_hello" {
+  rest_api_id = aws_api_gateway_rest_api.test_api.id
+  resource_id = aws_api_gateway_resource.test_resource.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "options_integration" {
+   rest_api_id = aws_api_gateway_rest_api.test_api.id
+  resource_id = aws_api_gateway_resource.test_resource.id
+  http_method = aws_api_gateway_method.options_hello.http_method
+  type        = "MOCK"
+
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+
+# 6️⃣ Add Method Response (so CORS headers appear)
+resource "aws_api_gateway_method_response" "options_response" {
+  rest_api_id = aws_api_gateway_rest_api.test_api.id
+  resource_id = aws_api_gateway_resource.test_resource.id
+  http_method = aws_api_gateway_method.options_hello.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+}
+
+
+# 7️⃣ Integration Response with CORS headers
+resource "aws_api_gateway_integration_response" "options_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.test_api.id
+  resource_id = aws_api_gateway_resource.test_resource.id
+  http_method = aws_api_gateway_method.options_hello.http_method
+  status_code = aws_api_gateway_method_response.options_response.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+}
+
 resource "aws_api_gateway_deployment" "test_deploy" {
     rest_api_id = aws_api_gateway_rest_api.test_api.id
 
@@ -37,6 +87,10 @@ resource "aws_api_gateway_deployment" "test_deploy" {
         aws_api_gateway_integration.test_int.id,
         aws_api_gateway_method.test_method.id,
         aws_api_gateway_resource.test_resource.id,
+        aws_api_gateway_method.options_hello.id,
+        aws_api_gateway_integration.options_integration.id,
+        aws_api_gateway_method_response.options_response.id,
+        aws_api_gateway_integration_response.options_integration_response.id
       ]))
     }
 
